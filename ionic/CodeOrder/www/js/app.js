3ea5cbb8439abb6d8e436d485db608fc2fa57ccb
netoudi <netoudi@rocketmail.com>
2016-08-02 17:30:12 -0300

    Installing and configuration of the angular-oauth2.

--- ionic/CodeOrder/www/js/app.js
+++ ionic/CodeOrder/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'angular-oauth2'])
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -23,7 +23,22 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     });
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, OAuthProvider, OAuthTokenProvider) {
+
+    OAuthProvider.configure({
+      baseUrl: 'http://localhost:8888',
+      clientId: 'testclient',
+      clientSecret: 'testpass',
+      grantPath: '/oauth',
+      revokePath: '/oauth'
+    });
+
+    OAuthTokenProvider.configure({
+      name: 'token',
+      options: {
+        secure: false
+      }
+    });
 
     $stateProvider
       .state('login', {
--- ionic/CodeOrder/www/js/controllers.js
+++ ionic/CodeOrder/www/js/controllers.js
@@ -1,24 +1,13 @@
 angular.module('starter.controllers', [])
 
   .controller('LoginCtrl', [
-    '$scope', '$http', '$state',
-    function ($scope, $http, $state) {
+    '$scope', '$http', '$state', 'OAuth',
+    function ($scope, $http, $state, OAuth) {
       $scope.login = function (data) {
-        var dataPost = {
-          grant_type: 'password',
-          client_id: 'testclient',
-          client_secret: 'testpass',
-          username: data.username,
-          password: data.password
-        };
-
-        $http.post('http://localhost:8888/oauth', dataPost)
-          .success(function (data) {
-            localStorage.setItem('order_token', data.access_token);
-            localStorage.setItem('order_refresh_token', data.refresh_token);
+        OAuth.getAccessToken(data)
+          .then(function (data) {
             $state.go('tabs.orders');
-          })
-          .error(function (error) {
+          }, function (error) {
             $scope.error_login = 'Username or password is invalid.'
           });
       }
